9d21655eba5622f29f32040647100b196c5a66a2
takadev <nontan20xx@gmail.com>
2018-07-31 00:36:08 +0900

    Modify dining philosophers

--- dining_philosophers/src/main.rs
+++ dining_philosophers/src/main.rs
@@ -1,3 +1,6 @@
+use std::thread;
+use std::time::Duration;
+
 struct Philosopher {
     name: String,
 }
@@ -10,6 +13,10 @@ impl Philosopher {
     }
 
     fn eat(&self) {
+        println!("{} is eating.", self.name);
+
+        thread::sleep(Duration::from_millis(1000));
+
         println!("{} is done eating.", self.name);
     }
 }
@@ -31,7 +38,19 @@ fn main() {
         Philosopher::new("Michel Foucault"),
     ];
 
+    let handles: Vec<_> = philosophers.into_iter().map(|p| {
+        thread::spawn(move || {
+            p.eat();
+        })
+    }).collect();
+
+    for h in handles {
+        h.join().unwrap();
+    }
+
+    /*
     for p in &philosophers {
         p.eat();
     }
+    */
 }
