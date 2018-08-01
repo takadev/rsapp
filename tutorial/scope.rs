0a76f540c0365f2d8d185db13ca062e990e1562a
takadev <nontan20xx@gmail.com>
2018-08-02 00:33:46 +0900

    Update README.md

---
+++ tutorial/scope.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let x: i32 = 17;
+    {
+        let y: i32 = 3;
+        println!("The value of x is {} and value of y is {}", x, y);
+    }
+    println!("The value of x is {} and value of y is {}", x, y); // これは動きません
+}
